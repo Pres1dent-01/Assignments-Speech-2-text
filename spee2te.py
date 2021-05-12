a59df446fa49a93239bb6c3f3b2678fd964be2d9
Vale <51172366+Pres1dent-01@users.noreply.github.com>
2021-05-12 19:36:01 +0530

    Few minor changes

--- spee2te.py
+++ spee2te.py
@@ -1,12 +1,12 @@
 import speech_recognition as sr
 print("Bhauk!!")
 
-r = sr.Recognizer()
+recon = sr.Recognizer()
 
 with sr.Microphone() as source:
-    audio_text = r.listen(source)
+    audio_text = recon.listen(source)
     print("Bas aur mat bhauk!")    
     try:
-        print("Text: "+r.recognize_google(audio_text))
+        print("Text: "+recon.recognize_google(audio_text))
     except:
          print("Kya bhauka")
